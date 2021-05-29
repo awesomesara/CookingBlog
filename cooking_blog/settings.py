f9ac83a8d34715a02da5a91870ff3d4c94900c14
Sara <medetovasara@gmail.com>
2021-05-29 16:51:03 +0600

    join git ignore

--- cooking_blog/settings.py
+++ cooking_blog/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kpp%sy%kehh$8&cppazp$ev)k71y++aoc$4tgv=ecckxli0x+u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,11 +83,11 @@ WSGI_APPLICATION = 'cooking_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cooking_blog_db',
-        'USER': 'sara',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        "NAME": config('DB_NAME'),
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
+        "HOST": config('DB_HOST'),
+        "PORT": config('DB_PORT', cast=int),
     }
 }
 
